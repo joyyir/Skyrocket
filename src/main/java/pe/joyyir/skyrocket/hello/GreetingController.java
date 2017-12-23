54468002595b02c819e3e00dc9b05721ae1ab189
joyyir <joyyir@naver.com>
2017-12-24 02:14:56 +0900

    deploy test 2

--- src/main/java/pe/joyyir/skyrocket/hello/GreetingController.java
+++ src/main/java/pe/joyyir/skyrocket/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Merry Christmas, %s!";
+    private static final String template = "Merry Christmas and Happy New Year, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
